67bb9469b7ab2b0c122a8662661ae2717867ec40
praveen970 <praveeneyeruva970@gmail.com>
2017-10-12 22:31:48 -0700

    added database

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var app = express.Router(); 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,3 +8,15 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+var pg = require('pg');
+  app.get('/db', function (request, response) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        client.query('SELECT * FROM test_table', function(err, result) {
+            done();
+            if (err)
+            { console.error(err); response.send("Error " + err); }
+            else
+            { response.render('pages/db', {results: result.rows} ); }
+        });
+    });
+});
