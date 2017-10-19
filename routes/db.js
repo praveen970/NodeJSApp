0046fde48aa57dedb5aa6e85edf6aa9850181911
praveen970 <praveeneyeruva970@gmail.com>
2017-10-19 12:54:27 -0700

    Add js file for db

--- routes/db.js
+++ routes/db.js
@@ -2,9 +2,36 @@ var express = require('express');
 var router = express.Router();
 
 
-/* GET home page. */
-router.get('/db', function(req, res, next) {
+router.get('/', function(req, res, next) {
+    res.render('db', { title: 'HelloWorld' });
+});
+module.exports = router;
+/* GET db page.
+var pg = require('pg');
+router.get('/', function(req, res, next) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        client.query('SELECT * FROM test_table', function (err, result) {
+                done();
+                res.render('db',{results: result.rows});
+            }
+        )
+    })
     res.render('db', { title: 'HelloWorld' });
 });
 
-module.exports = router;
+module.exports = router;*/
+
+//var app = express();
+/*var pg = require('pg');
+router.get('/db', function (request, response) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        client.query('SELECT * FROM test_table', function(err, result) {
+            done();
+            if (err)
+            { console.error(err); response.send("Error " + err); }
+            else
+            { response.render('db', {results: result.rows} ); }
+        });
+    });
+});
+module.exports = router;*/
