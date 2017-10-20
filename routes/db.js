27b81ccaafee5faaaef1c3b8491177dc30bb8cb8
praveen970 <praveeneyeruva970@gmail.com>
2017-10-19 22:30:44 -0700

    heroku db commit 1 conf need to be changed

--- routes/db.js
+++ routes/db.js
@@ -2,15 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 var pg = require('pg');
-var conString = 'postgres:praveen:password@//localhost:5432/pkdb';
-
-var config = {
+//Heroku
+//var conString = 'postgres:ikuxfhdkamnzyy:40fd460472c1e0cc365ebdb619d1a1ceba6a4112442d5d8fc2565a18c782f197@//ec2-23-23-192-242.compute-1.amazonaws.com:5432/d9nn2dsj7hs6st';
+var conString = 'postgres://ikuxfhdkamnzyy:40fd460472c1e0cc365ebdb619d1a1ceba6a4112442d5d8fc2565a18c782f197@ec2-23-23-192-242.compute-1.amazonaws.com:5432/d9nn2dsj7hs6st'
+//Local
+/*var config = {
     user : 'praveen',
     database : 'pkdb',
     max : 10,
     idleTimeOutMillis : 3000
-}
-var client = new pg.Client(config);
+}*/
+var client = new pg.Client(conString);
 router.get('/', function(req, res, next) {
     client.connect(function (err) {
         if (err)
